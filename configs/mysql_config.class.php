547f4ceb435612de0a3bc54dfc0171e278491ac1
Ahmosys <82736431+Ahmosys@users.noreply.github.com>
2022-05-23 20:36:59 +0200

    🌟 Added SQL Config Class

---
+++ configs/mysql_config.class.php
@@ -0,0 +1,12 @@
+<?php
+
+class MysqlConfig {
+
+    const SERVEUR = "YOUR_SERVER_NAME";
+    const BASE = "YOUR_DATABASE_NAME";
+    const UTILISATEUR = "YOUR_USERNAME";
+    const MOT_DE_PASSE = "YOUR_PASSWORD";
+    
+}
+
+?>
